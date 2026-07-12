f2f1056b56f7fe2f15ff6dc42b56062f96c402ac
Naincy <68952288+Naincy33@users.noreply.github.com>
2026-07-12 14:26:05 +0530

    Time: 55 ms (31.28%), Space: 28.1 MB (46.5%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayRankTransform(self, arr):
+        sorted_arr = sorted(arr)
+
+        rank = {}
+        r = 1
+
+        for num in sorted_arr:
+            if num not in rank:
+                rank[num] = r
+                r += 1
+
+        return [rank[num] for num in arr]
