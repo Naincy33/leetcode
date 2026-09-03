aac3c4f6585a93a47b3fe74ff679d2eca34b1444
Naincy <68952288+Naincy33@users.noreply.github.com>
2026-09-03 18:46:44 +0530

    Time: 151 ms (15.38%), Space: 21.7 MB (61.54%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniformArray(self, nums1):
+        min_odd = float('inf')
+        min_even = float('inf')
+
+        for x in nums1:
+            if x % 2 == 0:
+                min_even = min(min_even, x)
+            else:
+                min_odd = min(min_odd, x)
+
+        # Already uniform parity
+        if min_odd == float('inf') or min_even == float('inf'):
+            return True
+
+        # Every even number can subtract the smallest odd
+        return min_odd < min_even
